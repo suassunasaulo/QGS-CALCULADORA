43b66e7b0ee874b2696d58c0f743d7583bb2e1de
suassunasaulo <saulosuassuna123@gmail.com>
2023-11-12 19:25:08 -0300

    atualização do arquivo

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -1,4 +1,18 @@
 
 public class Calculadora {
+	    public int subtraInteiros(int a, int b) {
+	        return a - b;
+	    }
 
-}
+	    public int multiplicaInteiros(int a, int b) {
+	        return a * b;
+	    }
+
+	    public int dividiInteiros(int a, int b) {
+	        if (b == 0) {
+	            throw new IllegalArgumentException("Não é possível dividir por zero");
+	        }
+	        return a / b;
+	    }
+	}
+
